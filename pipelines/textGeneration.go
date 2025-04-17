66f890378cb8bf0a7a4bc18881b7968f7950b9bc
Rada Florin Daniel <florindaniel.rada@gameloft.com>
2025-04-17 18:18:55 +0300

    Adding some code for implementing a TextGeneration pipeline to be used with LLMs

---
+++ pipelines/textGeneration.go
@@ -0,0 +1,98 @@
+package pipelines
+
+import (
+	"fmt"
+	"math"
+	"sync/atomic"
+	"time"
+
+	"github.com/knights-analytics/hugot/options"
+	"github.com/knights-analytics/hugot/pipelineBackends"
+)
+
+type TextGenerationPipeline struct {
+	*pipelineBackends.BasePipeline
+}
+
+type Chat struct {
+	Messages []string `json:"messages"`
+}
+
+func NewTextGenerationPipeline(config pipelineBackends.PipelineConfig[*TextGenerationPipeline], s *options.Options, model *pipelineBackends.Model) (*TextGenerationPipeline, error) {
+	defaultPipeline, err := pipelineBackends.NewBasePipeline(config, s, model)
+	if err != nil {
+		return nil, err
+	}
+	pipeline := &TextGenerationPipeline{BasePipeline: defaultPipeline}
+	for _, o := range config.Options {
+		o(pipeline)
+	}
+
+	return pipeline, nil
+}
+
+func (p *TextGenerationPipeline) GetModel() *pipelineBackends.Model {
+	return p.BasePipeline.Model
+}
+
+func (p *TextGenerationPipeline) GetMetadata() pipelineBackends.PipelineMetadata {
+	return pipelineBackends.PipelineMetadata{
+		OutputsInfo: []pipelineBackends.OutputInfo{
+			{
+				Name:       p.Model.OutputsMeta[0].Name,
+				Dimensions: p.Model.OutputsMeta[0].Dimensions,
+			},
+		},
+	}
+}
+
+func (p *TextGenerationPipeline) GetStats() []string {
+	return []string{
+		fmt.Sprintf("Statistics for pipeline: %s", p.PipelineName),
+		fmt.Sprintf("Tokenizer: Total time=%s, Execution count=%d, Average query time=%s",
+			time.Duration(p.Model.Tokenizer.TokenizerTimings.TotalNS),
+			p.Model.Tokenizer.TokenizerTimings.NumCalls,
+			time.Duration(float64(p.Model.Tokenizer.TokenizerTimings.TotalNS)/math.Max(1, float64(p.Model.Tokenizer.TokenizerTimings.NumCalls)))),
+		fmt.Sprintf("ONNX: Total time=%s, Execution count=%d, Average query time=%s",
+			time.Duration(p.PipelineTimings.TotalNS),
+			p.PipelineTimings.NumCalls,
+			time.Duration(float64(p.PipelineTimings.TotalNS)/math.Max(1, float64(p.PipelineTimings.NumCalls)))),
+	}
+}
+
+func (p *TextGenerationPipeline) Preprocess(batch *pipelineBackends.PipelineBatch, inputs []string) error {
+	start := time.Now()
+	pipelineBackends.TokenizeInputs(batch, p.Model.Tokenizer, inputs)
+	atomic.AddUint64(&p.Model.Tokenizer.TokenizerTimings.NumCalls, 1)
+	atomic.AddUint64(&p.Model.Tokenizer.TokenizerTimings.TotalNS, uint64(time.Since(start)))
+	err := pipelineBackends.CreateInputTensors(batch, p.Model.InputsMeta, p.Runtime)
+	return err
+}
+
+// Forward performs the forward inference of the feature extraction pipeline.
+func (p *TextGenerationPipeline) Forward(batch *pipelineBackends.PipelineBatch) error {
+	start := time.Now()
+	err := pipelineBackends.RunSessionOnBatch(batch, p.BasePipeline)
+	if err != nil {
+		return err
+	}
+	atomic.AddUint64(&p.PipelineTimings.NumCalls, 1)
+	atomic.AddUint64(&p.PipelineTimings.TotalNS, uint64(time.Since(start)))
+	return nil
+}
+
+func (p *TextGenerationPipeline) Postprocess(batch *pipelineBackends.PipelineBatch) error {
+	return nil
+}
+
+func (p *TextGenerationPipeline) Validate() error {
+	return nil
+}
+
+func (p *TextGenerationPipeline) Run(inputs []string) (pipelineBackends.PipelineBatchOutput, error) {
+	return nil, nil
+}
+
+func (p *TextGenerationPipeline) RunPipeline(inputs []string) ([]string, error) {
+	return []string{}, nil
+}
